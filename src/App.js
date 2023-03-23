344404a848a3ecc421d857ee42a43eb223b46eda
Akshay Gujjar <agujjar2206@gmail.com>
2023-03-22 22:11:17 -0700

    Task List App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+  const [inputValue, setInputValue] = useState('');
+  const [filter, setFilter] = useState('all');
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleAddTask = () => {
+    if (inputValue.trim()) {
+      setTasks([...tasks, { text: inputValue, completed: false }]);
+      setInputValue('');
+    }
+  };
+
+  const handleToggleTask = (index) => {
+    const newTasks = tasks.slice();
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = tasks.slice();
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleClearCompleted = () => {
+    setTasks(tasks.filter(task => !task.completed));
+  };
+
+  const filteredTasks = tasks.filter(task => {
+    if (filter === 'completed') return task.completed;
+    if (filter === 'active') return !task.completed;
+    return true;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Task List</h1>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter a task"
+        />
+        <button className="add-task" onClick={handleAddTask}>
+          Add Task
+        </button>
+      </div>
+      <div className="button-group">
+        <button className="all" onClick={() => setFilter('all')}>
+          All
+        </button>
+        <button
+          className="completed"
+          onClick={() => setFilter('completed')}
         >
-          Learn React
-        </a>
-      </header>
+          Completed
+        </button>
+        <button className="active" onClick={() => setFilter('active')}>
+          Active
+        </button>
+        <button className="clear" onClick={handleClearCompleted}>
+          Clear Completed
+        </button>
+      </div>
+      <ul>
+      {filteredTasks.map((task, index) => (
+  <li 
+    key={index}
+    onClick={() => handleToggleTask(index)}
+  >
+    <span
+      style={{
+        textDecoration: task.completed ? 'line-through' : 'none',
+      }}
+    >
+      {task.text}
+    </span>
+    <button
+      className="delete"
+      onClick={(event) => {
+        event.stopPropagation();
+        handleDeleteTask(index);
+      }}
+    >
+      Delete
+    </button>
+  </li>
+))}
+
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
